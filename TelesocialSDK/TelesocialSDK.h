6e2992cc236fef31fa22f787255d263d2e216987
Anton Minin <anton.minin@gmail.com>
2011-08-20 00:42:55 +0300

    add README file; add TelesocialSDK folder - pre-compiled SDK, ready for integration

---
+++ TelesocialSDK/TelesocialSDK.h
@@ -0,0 +1,13 @@
+/*
+ *  TelesocialSDK.h
+ *  BitmouthSDK
+ *
+ *  Created by Anton Minin on 8/13/11.
+ *  Copyright 2011 UMITI. All rights reserved.
+ *
+ */
+
+#import "TSRestClient.h"
+#import "TSRestClientDelegate.h"
+#import "TSStatus.h"
+#import "TSMediaInfo.h"
